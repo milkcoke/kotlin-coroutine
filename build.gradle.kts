97fdb4a822395dc9b475a2c060ba117ab49a7fa1
milkcoke <mbh023@gmail.com>
2026-03-18 10:45:17 +0900

    + build: Update the build.gradle.kts
    - Update the dependencies to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  kotlin("jvm") version "2.3.0"
+  kotlin("jvm") version "2.3.20"
 }
 
 group = "coroutine"
@@ -12,10 +12,10 @@ repositories {
 dependencies {
   implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 
-  testImplementation(platform("org.junit:junit-bom:6.0.1"))
+  testImplementation(platform("org.junit:junit-bom:6.0.3"))
   testImplementation("org.junit.jupiter:junit-jupiter")
-  testImplementation("org.assertj:assertj-core:3.27.6")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:6.1.0")
+  testImplementation("org.assertj:assertj-core:3.27.7")
+  testImplementation("org.mockito.kotlin:mockito-kotlin:6.2.3")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 }
 
